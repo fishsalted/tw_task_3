a6d8ce6579eec3dfbc6e195bd90fa2921a8bec48
fishsalted <10572677+fishsalted@users.noreply.github.com>
2019-03-30 10:36:09 +0800

    完成section2

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,12 +1,19 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
+        Map<String,Integer> result = new HashMap<>();
 
-        return null;
+        for (String collection1_item : collection1) {
+            Integer value = result.get(collection1_item);
+            value = value == null ? 1 : value + 1;
+            result.put(collection1_item, value);
+        }
+
+        return result;
     }
 }
--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,33 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
+        Map<String,Integer> result = new HashMap<>();
 
-        return null;
+        for (String collection1_item : collection1) {
+            Integer value = 1;
+            int split_index = collection1_item.indexOf('-');
+            if(split_index == -1) {
+                if(-1 == (split_index = collection1_item.indexOf(':'))) {
+                    if(-1 != (split_index = collection1_item.indexOf('['))) {
+                        collection1_item = collection1_item.substring(0,collection1_item.length() - 1);
+                    }
+                }
+            }
+            if(split_index != -1) {
+                value = Integer.parseInt(collection1_item.substring(split_index + 1));
+                collection1_item = collection1_item.substring(0, split_index);
+            }
+            if(result.containsKey(collection1_item)) {
+                value += result.get(collection1_item);
+            }
+            result.put(collection1_item, value);
+        }
+
+        return result;
     }
 }
